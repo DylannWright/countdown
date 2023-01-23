f1f48a312d15fa819ac009806ca88a81fe788ec0
Dylan Wright <17WrighD@fulford.york.sch.uk>
2023-01-23 15:11:26 +0000

    added comments

--- lib.js
+++ lib.js
@@ -5,14 +5,14 @@ const elements = {
 }
 
 let running = false;
-
+//function to start the timer
 function start() {
     running = !running;
     elements.start.innerText = running ? "Stop" : "Start";
     console.log(running);
 }
 
-
+//function to reset the timer
 function reset() {
     timer.value = "00:00:00";
     clearInterval()
